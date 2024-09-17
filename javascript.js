5986224fb00c87071c2491c6ffd4c25d56077318
psychacker <psych4cker@gmail.com>
2024-09-17 18:11:39 +0530

    edit js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,34 @@
-function greet(name){
-    console.log("hlo");
-}
-greet();
+//---- //--- //-- This is a function --// ---// ----//
+
+// function greet(name){
+//     console.log("Hlo "+ name);
+// }
+// greet("sir");
+
+console.log('first');
+
+// function add(a,b){
+//     let total = a + b;
+//     console.log(total);
+// }
+// add(4,4);
+
+
+// const add = (o, t) => {return o + t;};
+// console.log(add(3,3));
+
+
+// function add(o, t, total) {
+//     let a = o + t;
+//     total(a);
+//     return () => {console.log(a)};
+//     // return o + t;
+// }
+
+// add(3,3, (aa)=>{console.log(aa);});
+
+// let lastFun = add(5,5, () => {});
+// lastFun();
+
+
+console.log('last');
