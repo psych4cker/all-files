5589426e2132e1d633bbbb969bdff72a622b27e4
psychacker <psych4ck3r@gmail.com>
2024-11-19 09:15:58 +0530

    edit

--- python.py
+++ python.py
@@ -1,6 +1,10 @@
 #---- #--- #-- This is a function --# ---# ----#
-def greet():
-    name = input("What's your name: ")
-    return print("WELCOME "+name +" welcome!!")
+# def greet():
+#     name = input("What's your name: ")
+#     return print("WELCOME "+name +" welcome!!")
 
-greet()
+# greet()
+
+for i in range(1, 100000000):
+    
+    print(i)
